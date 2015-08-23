688e3f2db3a88fa3ff04c6488afe38b8dd96f7dc
levithomason <me@levithomason.com>
2015-08-23 15:30:31 -0700

    wip, working on normalize range

--- test/Util_test.js
+++ test/Util_test.js
@@ -9,21 +9,27 @@ describe('Util', function() {
 
   describe('normalize', function() {
     it('scales an array of numbers to -1 and 1', function() {
-      var normalized = Util.normalize(_.range(-1000, 1000));
+      var normalized = Util.normalize(_.range(-100, 100));
       _.max(normalized).should.equal(1);
       _.min(normalized).should.equal(-1);
     });
 
     it('works with positive numbers', function() {
-      var normalized = Util.normalize(_.range(0, 1000));
+      var normalized = Util.normalize(_.range(0, 100));
       _.max(normalized).should.equal(1);
       _.min(normalized).should.equal(-1);
     });
 
     it('works with negative numbers', function() {
-      var normalized = Util.normalize(_.range(-1000, 0));
+      var normalized = Util.normalize(_.range(-100, 0));
       _.max(normalized).should.equal(1);
       _.min(normalized).should.equal(-1);
+    });
+
+    it('allows arbitrary min and max scale factors', function() {
+      var normalized = Util.normalize(_.range(-10, 10), -5, 5);
+      _.max(normalized).should.equal(2);
+      _.min(normalized).should.equal(-2);
     });
   });
 
