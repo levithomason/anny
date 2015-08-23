b3362a5fab8a3d38ba8ab87c2da1f4669f120e03
levithomason <me@levithomason.com>
2015-08-23 12:23:31 -0700

    scale data from -1 to +1

--- training-data/Data2048Scaled.js
+++ training-data/Data2048Scaled.js
@@ -1,18 +1,21 @@
-var data2048Scaled = _.map(data2048, function(game) {
-  return _.map(game.data, function(move) {
-    var grid = move.input;
-    var direction = move.output;
-    return {
-      input: move.input,
-      output: move.output
-    };
-  });
-});
+var data2048Scaled = _(data2048)
+  .pluck('data')
+  .flatten()
+  .thru(function(turns) {
+    // find largest tile
+    var largestTile = _.max(_.flatten(_.pluck(turns, 'input')));
 
-// shuffle the dataset
-data2048Scaled = _.shuffle(data2048Scaled);
+    // scale inputs to (-1, +1) by largest tile
+    return turns.map(function(turn) {
+      turn.input = anny.util.normalize(turn.input, 0, largestTile);
+      return turn;
+    });
+  })
+  .shuffle()
+  .value();
+
+console.log(data2048Scaled);
 
 var dataBreakPoint = Math.floor(data2048Scaled.length * 0.9);
-
 var data2048ScaledTraining = _.slice(data2048Scaled, 0, dataBreakPoint);
 var data2048ScaledTest = _.slice(data2048Scaled, dataBreakPoint, data2048Scaled.length);
