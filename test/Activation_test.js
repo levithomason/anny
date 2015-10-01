e51721843a8412c497a098b1694463907b7e5b9f
levithomason <me@levithomason.com>
2015-09-30 23:34:35 -0500

    restructure activation object

--- test/Activation_test.js
+++ test/Activation_test.js
@@ -1,67 +1,62 @@
 var ACTIVATION = require('../lib/Activation');
 
 describe('ACTIVATION', function() {
-  it('contains only function properties', function() {
-    _.each(ACTIVATION, function(fn) {
-      fn.should.be.a('function');
+  it('is an object', function() {
+    ACTIVATION.should.be.a('object');
+  });
+
+  it('has only object values', function() {
+    _.each(ACTIVATION, function(val, key) {
+      val.should.be.a('object');
     });
   });
 
-  it('has no anonymous functions', function() {
-    _.each(ACTIVATION, function(fn, name) {
-      fn.name.should.equal(name);
+  it('has object values with a `normal` method', function() {
+    _.each(ACTIVATION, function(val, key) {
+      val.normal.should.be.a('function');
     });
   });
 
-  it('has methods that return numbers', function() {
-    _.each(ACTIVATION, function(fn) {
-      fn(_.random(-1, 1, true)).should.be.a('number');
+  it('has object values with a `prime` method', function() {
+    _.each(ACTIVATION, function(val, key) {
+      val.prime.should.be.a('function');
     });
   });
 
-  it('includes a derivative for every function', function() {
-    _.each(ACTIVATION, function(fn, name) {
-      if (!_.contains(name, 'Derivative')) {
-        ACTIVATION[name + 'Derivative'].should.be.a('function');
-      }
-    });
-  });
-
-  it('includes a function for every derivative ', function() {
-    _.each(ACTIVATION, function(fn, name) {
-      if (_.contains(name, 'Derivative')) {
-        ACTIVATION[name.replace('Derivative', '')].should.be.a('function');
-      }
+  it('has `normal` and `prime` methods return numbers', function() {
+    _.each(ACTIVATION, function(val, key) {
+      val.normal(_.random(-100, 100, true)).should.be.a('number');
+      val.prime(_.random(-100, 100, true)).should.be.a('number');
     });
   });
 
   describe('rectifier', function() {
     it('returns 0 if x is <= 0', function() {
-      ACTIVATION.rectifier(-_.random(100, true)).should.equal(0);
-      ACTIVATION.rectifier(0).should.equal(0);
+      ACTIVATION.rectifier.normal(-_.random(100, true)).should.equal(0);
+      ACTIVATION.rectifier.normal(0).should.equal(0);
     });
 
     it('returns x if x is > 0', function() {
       var x = _.random(100);
-      ACTIVATION.rectifier(x).should.equal(x);
+      ACTIVATION.rectifier.normal(x).should.equal(x);
     });
   });
 
   describe('softplus', function() {
     it('returns 0 if x is very negative', function() {
-      ACTIVATION.softplus(-37).should.equal(0);
+      ACTIVATION.softplus.normal(-37).should.equal(0);
     });
 
     it('returns approx of x if x is greater than 0', function() {
       var x = _.random(100, true);
-      ACTIVATION.softplus(x).should.be.gte(x);
+      ACTIVATION.softplus.normal(x).should.be.gte(x);
     });
   });
 
   describe('identity', function() {
     it('returns x', function() {
       var x = _.random(-1, 1, true);
-      ACTIVATION.identity(x).should.equal(x);
+      ACTIVATION.identity.normal(x).should.equal(x);
     });
   });
 });
