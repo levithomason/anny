531a73570055cd69ced95446fa05d5ef8a7b95d0
levithomason <me@levithomason.com>
2015-11-21 17:58:18 -0800

    add activation ranges

--- src/Activation.js
+++ src/Activation.js
@@ -8,30 +8,32 @@ import math from 'mathjs';
 const ACTIVATION = {
   /**
    * Simply max(0, x). Interestingly the derivative of the rectifier turns out
-   * to be the logistic function. Range: (0,+inf)
+   * to be the logistic function.
    * @param x
    */
   rectifier: {
     // https://en.wikipedia.org/wiki/Rectifier
     func: x => math.max(0, x),
     prime: x => 1 / (1 + math.exp(-x)),
+    rangeMin: 0,
+    rangeMax: Infinity,
   },
 
   /**
    * A smooth approximation of the rectifier.
-   * Rage: (0,+inf)
    * @param x
    */
   softplus: {
     // https://en.wikipedia.org/wiki/Rectifier_(neural_networks)
     func: x => math.log(1 + math.exp(x)),
     prime: x => math.log(1 + math.exp(x)),
+    rangeMin: 0,
+    rangeMax: Infinity,
   },
 
   /**
    * A smoothed step function or an 'S' shape. Also called the sigmoid
    * function, though there are many sigmoid functions.
-   * Range: (0,+1)
    * @param {number} x
    * @returns {number}
    */
@@ -42,17 +44,20 @@ const ACTIVATION = {
       const val = 1 / (1 + math.exp(-x));
       return val * (1 - val);
     },
+    rangeMin: 0,
+    rangeMax: 1,
   },
 
   /**
    * Simply passes the input to the output with no transformation.
-   * Range: (-inf,+inf)
    * @param {number} x
    * @returns {number}
    */
   identity: {
     func: x => x,
-    prime: x => x,
+    prime: x => 1,
+    rangeMin: -Infinity,
+    rangeMax: Infinity,
   },
 
   /**
@@ -60,7 +65,6 @@ const ACTIVATION = {
    * function, except it has a range of (-1,+1). Often performs better than
    * the logistic function because of its symmetry. Ideal for customization of
    * multilayer perceptrons, particularly the hidden layers.
-   * Range: (-1, +1)
    * @param {number} x
    * @returns {number}
    */
@@ -71,17 +75,20 @@ const ACTIVATION = {
       return (posExp - negExp) / (posExp + negExp);
     },
     prime: x => 1 - math.pow(math.tanh(x), 2),
+    rangeMin: -1,
+    rangeMax: 1,
   },
 
   /**
    * Modified hyperbolic tangent function.  Optimized for faster convergence.
-   * Range: (-1, +1)
    * @param {number} x
    * @returns {number}
    */
   optimalTanh: {
     func: x => 1.7159 * math.tanh(x * 2 / 3),
     prime: x => 1.14393 * math.sech(x * 2 / 3),
+    rangeMin: -1,
+    rangeMax: 1,
   },
 };
 
