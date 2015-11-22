5032c2d474233d18989fcd811ff3aeeb0e353d5e
levithomason <me@levithomason.com>
2015-11-21 17:58:18 -0800

    add activation ranges

--- test/Activation_test.js
+++ test/Activation_test.js
@@ -8,48 +8,23 @@ describe('ACTIVATION', () => {
     _.each(ACTIVATION, val => val.should.be.a('object'));
   });
 
-  it('has object values with a `func` method', () => {
-    _.each(ACTIVATION, val => val.func.should.be.a('function'));
-  });
+  describe('activation object', () => {
+    _.each(ACTIVATION, object => {
+      it('has a `func` method that returns a number', () => {
+        object.func(_.random(-100, 100, true)).should.be.a('number');
+      });
 
-  it('has object values with a `prime` method', () => {
-    _.each(ACTIVATION, val => val.prime.should.be.a('function'));
-  });
+      it('has a `prime` method that returns a number', () => {
+        object.prime(_.random(-100, 100, true)).should.be.a('number');
+      });
 
-  it('has `func` and `prime` methods return numbers', () => {
-    _.each(ACTIVATION, val => {
-      val.func(_.random(-100, 100, true)).should.be.a('number');
-      val.prime(_.random(-100, 100, true)).should.be.a('number');
-    });
-  });
+      it('has a `rangeMin` number property', () => {
+        expect(object.rangeMin).to.be.a('number');
+      });
 
-  describe('rectifier', () => {
-    it('returns 0 if x is <= 0', () => {
-      ACTIVATION.rectifier.func(-_.random(100, true)).should.equal(0);
-      ACTIVATION.rectifier.func(0).should.equal(0);
-    });
-
-    it('returns x if x is > 0', () => {
-      const x = _.random(100);
-      ACTIVATION.rectifier.func(x).should.equal(x);
-    });
-  });
-
-  describe('softplus', () => {
-    it('returns 0 if x is very negative', () => {
-      ACTIVATION.softplus.func(-37).should.equal(0);
-    });
-
-    it('returns approx of x if x is greater than 0', () => {
-      const x = _.random(100, true);
-      ACTIVATION.softplus.func(x).should.be.gte(x);
-    });
-  });
-
-  describe('identity', () => {
-    it('returns x', () => {
-      const x = _.random(-1, 1, true);
-      ACTIVATION.identity.func(x).should.equal(x);
+      it('has a `rangeMax` number property', () => {
+        expect(object.rangeMax).to.be.a('number');
+      });
     });
   });
 });
