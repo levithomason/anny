646fbf8d717378821dae0541b2639d94be455fec
levithomason <me@levithomason.com>
2015-12-21 00:54:22 -0800

    add playground.js and npm run playground

--- playground.js
+++ playground.js
@@ -1,19 +1,18 @@
+/* eslint-disable no-console */
 import Network from './src/Network'
 import Data from './src/Data'
 
-const network = new Network([2, 5, 1])
+const network = new Network([4, 5, 3])
 
-network.train(Data.ORGate, {
-  frequency: 1,
+network.train(Data.irisFlower, {
+  frequency: 1000,
   onSuccess: (error, epoch) => {
     console.log(`Successfully trained to ${error} error after ${epoch} epochs`)
   },
   onFail: (error, epoch) => {
     console.log(`Fail to train, error is ${error} after ${epoch} epochs`)
   },
-  onUpdate: (error, epoch) => {
+  onProgress: (error, epoch) => {
     console.log(`${epoch} ${error}`)
-    // exit training
-    return false
   },
 })
